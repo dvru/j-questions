f100c5527efa3038040e6d607e6060f7340c5b6e
dvru <damvaru4@gmail.com>
2020-12-23 18:28:38 -0600

    started fizzbuzz

---
+++ Control Flow/fizzBuzz.js
@@ -0,0 +1,12 @@
+// Divisible by 3 => Fizz
+// Divisible by 5 => Buzz
+// Divisible by both 3 and 5 => FizzBuzz
+// Not divisible by 3 or 5 => input
+// Not a number => 'Not a number'
+
+const output = fizzBuzz(3);
+console.log(output);
+
+function fizzBuzz(input) {
+
+}
