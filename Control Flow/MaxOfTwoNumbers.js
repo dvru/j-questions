13de19a0a86b46d5238184e480ad785c3729768d
dvru <damvaru4@gmail.com>
2020-12-23 18:23:00 -0600

    Control Flow

---
+++ Control Flow/MaxOfTwoNumbers.js
@@ -0,0 +1,12 @@
+// Write a function that takes 2 numbers and returns the maximum of the two.
+
+let number = max(3, 3)
+console.log(number)
+
+function max(num1, num2){
+    // if(num1 > num2) return num1;
+    // return num2;
+
+    return(num1 > num2) ? num1 : num2;
+
+}
