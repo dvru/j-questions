aa08c52f336c9e5c25fb32c962e5ee2ba501f40a
dvru <damvaru4@gmail.com>
2020-12-25 21:57:07 -0600

    finishedfizzbuzz

--- Control Flow/fizzBuzz.js
+++ Control Flow/fizzBuzz.js
@@ -5,9 +5,21 @@
 // Not a number => 'Not a number'
 
 
-const output = fizzBuzz(3);
+const output = fizzBuzz(false);
 console.log(output);
 
 function fizzBuzz(input) {
-
-}
+ if(typeof input !== 'number'){
+    return NaN;
+ }
+ else if ((input % 3 === 0) && (input % 5 == 0)){
+    return 'FizzBuzz';
+ }
+ else if (input % 3 === 0){
+    return 'Fizz';
+ }
+ else if(input % 5 == 0){
+    return 'Buzz';
+ }
+    return input;
+ }
