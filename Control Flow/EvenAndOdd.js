9f952c3dab94888fa0e07318388fb8d05ecca99c
dvru <damvaru4@gmail.com>
2020-12-29 17:18:50 -0600

    finished demerit points

---
+++ Control Flow/EvenAndOdd.js
@@ -0,0 +1,13 @@
+showNumbers(20); // listing numbers from 0 - 10, even and odd, need a for loop to view the numbers from 0 - 10
+
+function showNumbers(limit){
+    for(let i = 0; i <= limit; i++){
+        // if(i % 2 === 0) 
+        // console.log(i, 'EVEN');
+        // else
+        //     console.log(i, 'ODD');
+
+        const message = (i % 2 === 0) ? 'EVEN' : 'ODD';
+        console.log(i, message);
+    }
+}
