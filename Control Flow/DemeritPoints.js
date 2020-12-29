b6124da095734fea79506cb385c1fcd326313238
dvru <damvaru4@gmail.com>
2020-12-29 16:09:03 -0600

    started demerit points

---
+++ Control Flow/DemeritPoints.js
@@ -0,0 +1,10 @@
+// Speed Limit = 70; if theyre driving under the speed limit, console.log 'ok';
+// 5 -> 1 point; for every 5 km above the speed limit, they will get 1 point;
+// Math.floor(1.3); // convert it to the greatest integer
+// 12 points -> suspended; more than 12 points = suspended
+
+checkSpeed(180);
+
+function checkSpeed(speed){
+
+}
