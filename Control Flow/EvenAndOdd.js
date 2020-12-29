093987edaacc7512cf8a3fd60698d110d67445e4
dvru <damvaru4@gmail.com>
2020-12-29 17:20:27 -0600

    finished even and odds

--- Control Flow/EvenAndOdd.js
+++ Control Flow/EvenAndOdd.js
@@ -1,5 +1,6 @@
 showNumbers(20); // listing numbers from 0 - 10, even and odd, need a for loop to view the numbers from 0 - 10
 
+
 function showNumbers(limit){
     for(let i = 0; i <= limit; i++){
         // if(i % 2 === 0) 
