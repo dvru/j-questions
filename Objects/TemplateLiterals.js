22b8b1739f7f74394798e1ce94b201cad7f68a30
dvru <damvaru4@gmail.com>
2020-12-30 15:07:16 -0600

    Date

--- Objects/TemplateLiterals.js
+++ Objects/TemplateLiterals.js
@@ -8,3 +8,16 @@ const another =
 first message`;
 
 console.log(another);
+
+
+const now = new Date();
+const date1 = new Date('May 11, 2018 09:00');
+const date2 = new Date(2018, 4, 11, 9, 0);
+
+
+console.log(now.toDateString()); //Wed Dec 30 2020
+console.log(now.toTimeString()); //15:06:25 GMT-0600 (Central Standard Time)
+
+now.setFullYear(2020);
+// Wed Dec 30 2020
+// 15:06:46 GMT-0600 (Central Standard Time)
