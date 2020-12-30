57c1d6a41daba0e7a957edba24a5eb3a67b239da
dvru <damvaru4@gmail.com>
2020-12-30 15:25:09 -0600

    factory and constructor functions

--- Objects/Exercises.js
+++ Objects/Exercises.js
@@ -2,7 +2,7 @@
 // address object with 3 prop, street, city, zipcode, 
 // function showAddress(address), display all props in object
 
-const address = {
+let address = {
     street: 'Hay Lane',
     city: 'London',
     zipcode: '77095'
@@ -17,3 +17,19 @@ function showAddress(address){
 showAddress(address);
 
 // Factory and Constructor Functions
+// initialize address object, using factory and constructor function
+
+
+// Factory
+function createAddress(address){
+    return {
+        address
+    }
+}
+console.log(address);
+
+// Constructor 
+function Address(address){
+    this.address = address;
+}
+console.log(address);
