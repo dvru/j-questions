ddc330b30170c51fdb9af2fede98c7dabdadd5ee
dvru <damvaru4@gmail.com>
2020-12-30 14:17:13 -0600

    prop of objects

---
+++ Objects/propOfObject.js
@@ -0,0 +1,23 @@
+const circle = {
+    radius: 1,
+    draw() {
+        console.log('draw');
+    }
+};
+
+for (let key in circle){
+    console.log(key); // radius, draw
+    console.log(key, circle[key]); // value of key in circle obj
+}
+
+// for of, only iterate within arrays, grab the keys of the object circle and return key
+for (let key of Object.keys(circle)){
+    console.log(key);
+}
+
+for (let entry of Object.entries(circle))
+    console.log(entry);
+// [ 'radius', 1 ]
+// [ 'draw', [Function: draw] ]
+
+if ('radius' in circle) console.log('yes')
