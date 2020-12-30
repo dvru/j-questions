2f35d2341ea45ebea6c57a0e77621baeefaf0db1
dvru <damvaru4@gmail.com>
2020-12-30 13:01:30 -0600

    started oop

--- Objects/basics.js
+++ Objects/basics.js
@@ -1,15 +1,30 @@
 // OOP: where we see a program as a collection of objects to talk to eachother to perform some sort of functionality
 
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    isVisible: true,
-    draw: function() {
-        console.log('draw');
-    }
-};
+// const circle = {
+//     radius: 1,
+//     location: {
+//         x: 1,
+//         y: 1
+//     },
+//     isVisible: true,
+//     draw: function() {
+//         console.log('draw');
+//     }
+// };
+// circle.draw();
 
-circle.draw();
+
+// Factory Function
+function createCircle(radius){
+    return {
+        radius,
+        draw() {
+            console.log('draw');
+        }
+    };
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+
+
