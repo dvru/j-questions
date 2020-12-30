f9c194b4ae5916d22c970ff10bfc692bd75e5171
dvru <damvaru4@gmail.com>
2020-12-30 13:18:48 -0600

    dynamic objects

--- Objects/basics.js
+++ Objects/basics.js
@@ -28,3 +28,14 @@ const circle1 = createCircle(1);
 console.log(circle1);
 
 
+// Constructor Function
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw');
+    }
+}
+   
+const circle = new Circle(1);
+
+
---
+++ Objects/dynamicNature.js
@@ -0,0 +1,8 @@
+// circle object with a single property, radius
+const circle = {
+    radius: 1
+}
+
+circle.color = 'yellow';
+
+console.log(circle);
