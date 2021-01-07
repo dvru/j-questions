344cdfb8c5c4da747658874fc8a573aa29b89122
dvru <damvaru4@gmail.com>
2021-01-07 11:29:26 -0600

    exercises

--- Arrays/Exercises.js
+++ Arrays/Exercises.js
@@ -3,7 +3,7 @@
 // empty array for initial array
 // for loop to loop though array
 
-const numbers = arrayFromRange(-10, -4);
+// const numbers = arrayFromRange(-10, -4);
 
 // console.log(numbers);
 
@@ -12,6 +12,20 @@ function arrayFromRange(min, max){
     for(let i = min; i <= max; i++)
         output.push(i);
         return console.log(output);
+}
+
+// Includes
+//create function called includes, takes an array and search element.
+// iterate through and check if that element is === to the search element return true
+const numbers = [1, 2, 3, 4];
+
+console.log(includes(numbers, 1))
+
+function includes(array, searchElement){
+    for(let element of array)
+        if (element === searchElement)
+            return true;
+        return false;
 }
 
 
