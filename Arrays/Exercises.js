cfb0e8c369977e824ae9425acdb66e8d4a297d7f
dvru <damvaru4@gmail.com>
2021-01-07 11:52:12 -0600

    exercises

--- Arrays/Exercises.js
+++ Arrays/Exercises.js
@@ -54,9 +54,16 @@ const output = move(numbers, 0, 1); // numbers array, first element, 1 postion t
 console.log(output);
 
 function move(array, index, offset){
+    const position = index + offset;
+    if(position >= array.length || position < 0){
+        console.error('Invalid Offset');
+        return;
+    }
+
+
     const output = [...array]; 
     const element = output.splice(index, 1)[0];
-    output.splice(index + offset, 0, element);
+    output.splice(position, 0, element);
     return output;
     
 }
