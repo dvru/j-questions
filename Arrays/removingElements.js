f70c6f70255f750a0c16527007de26aa293940e3
dvru <damvaru4@gmail.com>
2021-01-06 20:32:33 -0600

    removing elements solved

--- Arrays/removingElements.js
+++ Arrays/removingElements.js
@@ -1,7 +1,22 @@
 const ingredients = ['salt', 'pepper', 'spinach', 'mushrooms'];
 
-ingredients.push('garlic');
-ingredients.unshift('onions');
-ingredients.splice(2, 0, 'olives');
+// ingredients.push('garlic');
+// ingredients.unshift('onions');
+// ingredients.splice(2, 0, 'olives');
 
+
+// // Remove end
+// const last = ingredients.pop();
+// console.log(ingredients);
+// console.log(last);
+
+
+// // Remove beginning
+// const first = ingredients.shift();
+// console.log(ingredients);
+// console.log(first);
+
+// Remove Middle
+const middle = ingredients.splice(0, 2);
 console.log(ingredients);
+console.log(middle);
