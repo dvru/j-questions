ac3b1ae5c4ac2bf6f7bb378fccc188a37154bfe4
dvru <damvaru4@gmail.com>
2021-01-06 20:52:25 -0600

    spreadOp

---
+++ Arrays/iteratingJoiningSorting.js
@@ -0,0 +1,13 @@
+// Iterating through array
+
+const ingredients = ['salt', 'pepper', 'spinach', 'mushrooms'];
+
+for (let ingredient of ingredients){
+    console.log(ingredient);
+}
+
+ingredients.forEach(function(ingredient){
+    console.log(ingredient);
+})
+
+ingredients.forEach(ingredient => console.log(ingredient));
