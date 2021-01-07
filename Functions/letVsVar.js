afd9756bc694d7be1f83db92bf37706089e3be4f
dvru <damvaru4@gmail.com>
2021-01-07 17:49:24 -0600

    letvsvar

---
+++ Functions/letVsVar.js
@@ -0,0 +1,17 @@
+function start() {
+    for (var i = 0; i < 5; i++){
+        if(true){
+            var color = 'red';
+        }
+    }
+
+    console.log(color);
+};
+
+start();
+
+// let is not accessible outside the scope
+// var is accessable outside the scope
+
+// var => function scopr variables
+// ES6: let, const => block-scoped
