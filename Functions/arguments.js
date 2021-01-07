471277e94146e9f380a7967075212202f968730b
dvru <damvaru4@gmail.com>
2021-01-07 16:42:19 -0600

    arguments

---
+++ Functions/arguments.js
@@ -0,0 +1,17 @@
+function sum(){
+    let total = 0;
+    for(let value of arguments)
+        total += value;
+    return total;
+        
+
+
+    console.log(arguments)
+    return a + b; // 1 + undefinded = NaN
+}
+
+console.log(sum(1, 2, 3, 4, 5, 10));
+// console.log(sum(1)); // 1 + undefinded = NaN
+
+
+//
