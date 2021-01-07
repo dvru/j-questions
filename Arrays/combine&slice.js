0499c02ae9b5140db87e18b72512b10edf689ee7
dvru <damvaru4@gmail.com>
2021-01-06 20:42:54 -0600

    combine&slice

---
+++ Arrays/combine&slice.js
@@ -0,0 +1,10 @@
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+const combined = first.concat(second); // combine 2 arrays
+console.log(combined);
+
+const slice = combined.slice(2); // slice 2 arrays
+console.log(slice);
+
+
