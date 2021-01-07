65296c1855cfeaf484dd6e0ed058aaced75c39b8
dvru <damvaru4@gmail.com>
2021-01-06 21:05:01 -0600

    iteratingJoiningSorting

--- Arrays/iteratingJoiningSorting.js
+++ Arrays/iteratingJoiningSorting.js
@@ -2,6 +2,12 @@
 
 const ingredients = ['salt', 'pepper', 'spinach', 'mushrooms'];
 
+courses = [
+    {id: 1, name: 'Node.js'},
+    {id: 2, name: 'javaScript.js'}
+]
+
+
 for (let ingredient of ingredients){
     console.log(ingredient);
 }
@@ -11,3 +17,36 @@ ingredients.forEach(function(ingredient){
 })
 
 ingredients.forEach((ingredient, index) => console.log(index, ingredient));
+
+// Joining
+const join = ingredients.join(',');
+console.log(join);
+//salt,pepper,spinach,mushrooms
+
+const message = 'This is my message';
+const parts = message.split(' ');
+console.log(parts);
+//[ 'This', 'is', 'my', 'message' ]
+
+
+// Sorting
+ingredients.sort();
+console.log(ingredients); 
+// sorted alpha
+//[ 'mushrooms', 'pepper', 'salt', 'spinach' ]
+
+ingredients.reverse();
+console.log(ingredients); 
+//[ 'spinach', 'salt', 'pepper', 'mushrooms' ]
+
+courses.sort(function(first, second){
+
+    const nameFirst = first.name.toLowerCase();
+    const nameSecond = second.name.toLowerCase();
+
+    if (nameFirst < nameSecond) return -1;
+    if (nameFirst > nameSecond) return 1;
+    return 0;
+});
+console.log(courses);
+// [ { id: 2, name: 'JavaScript.js' }, { id: 1, name: 'Node.js' } ]
