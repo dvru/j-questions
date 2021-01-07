4572420e0e482c99500c205a0b90fc4748d1ef4d
dvru <damvaru4@gmail.com>
2021-01-06 20:47:13 -0600

    spreadOp

--- Arrays/spreadOp.js
+++ Arrays/spreadOp.js
@@ -1,6 +1,9 @@
 const first = [1, 2, 3];
 const second = [4, 5, 6];
 
-const combined = [...first, ...second]; // [ 1, 2, 3, 4, 5, 6 ]
+const combined = [...first, 'a', ...second]; // [ 1, 2, 3,'a', 4, 5, 6 ]
+
+const copy = [...combined];
 
 console.log(combined);
+console.log(copy);
