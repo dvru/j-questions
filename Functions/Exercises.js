d137c4e27994be10384600e2ba2082793e04cdba
dvru <damvaru4@gmail.com>
2021-01-07 18:23:45 -0600

    exercises

--- Functions/Exercises.js
+++ Functions/Exercises.js
@@ -8,3 +8,16 @@ function sum(...items) {
         items = [...items[0]]
     return items.reduce((a, b) => a + b);
 }
+
+// Area of Circle
+// circle.radius = 2;
+// console.log(circle.area);
+
+const circle = {
+    radius: 1,
+    get area(){
+        return Math.PI * this.radius * this.radius;
+    }
+};
+
+console.log(circle.area);
