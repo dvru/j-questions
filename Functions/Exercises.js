9c5da47b41503638b9b41b74805dcc310601fb4f
dvru <damvaru4@gmail.com>
2021-01-07 18:31:17 -0600

    exercises

--- Functions/Exercises.js
+++ Functions/Exercises.js
@@ -21,3 +21,26 @@ const circle = {
 };
 
 console.log(circle.area);
+
+
+
+// Error Handling
+try{
+    const numbers = (1, 2, 3, 4);
+    const count = countOccurances(true, 1);
+    console.log(count);
+}
+catch(e){
+    console.log(e.message);
+}
+
+function countOccurances(array, searchElement){
+   if(!Array.isArray(array))
+    throw new Error('Invalid array');
+
+    return array.reduce((accumulator, current) => {
+        const occurance = (current === searchElement) ? 1 : 0;
+        console.log(accumulator, current, searchElement);
+        return accumulator + occurance
+}, 0);
+}
