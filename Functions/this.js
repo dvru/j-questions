84fb37f5bac75e922436b201c09570d621b6dc24
dvru <damvaru4@gmail.com>
2021-01-07 18:16:28 -0600

    this

--- Functions/this.js
+++ Functions/this.js
@@ -16,3 +16,12 @@ function Video(title) {
 }
 
 const v = new Video('b');
+
+
+function playVideo(a, b) {
+    console.log(this);
+}
+
+playVideo.call({ name: 'Damini' }, 1, 2); // { name: 'Damini' }
+playVideo.apply({ name: 'Damini' }, [1, 2]);  // { name: 'Damini' }
+playVideo.bind({ name: 'Damini' })(); // { name: 'Damini' }
