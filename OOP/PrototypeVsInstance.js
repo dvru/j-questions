6f3d9f862fd94901e40f7f69b87f853df8ad43f8
dvru <damvaru4@gmail.com>
2021-01-18 17:15:15 -0600

    prototype bs instance

--- OOP/PrototypeVsInstance.js
+++ OOP/PrototypeVsInstance.js
@@ -15,3 +15,9 @@ const c2 = new Circle(1);
 Circle.prototype.toString = function() {
     return 'Circle with radius ' + this.radius; 
 }
+
+// Returns instance members
+console.log(Object.keys(c1));
+
+// Returns all members (instance + prototype)
+for (let key in c1) console.log(key);
