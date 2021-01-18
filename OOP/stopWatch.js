c5d437a9344101815d9f7cf7fef6a6ea06548c4f
dvru <damvaru4@gmail.com>
2021-01-18 14:53:21 -0600

    stopWatch

---
+++ OOP/stopWatch.js
@@ -0,0 +1,44 @@
+// const sw = new Stopwatch();, constructor function
+// sw.start
+// sw.stop
+// sw.reset
+// sw.duration = 0
+
+
+function Stopwatch() {
+    let startTime, endTime, running, duration = 0;
+
+    this.start = function () {
+        if (running)
+            throw new Error('Stopwatch has already started.');
+
+        running = true;
+
+        startTime = new Date();
+    };
+
+    this.stop = function () {
+        if (!running)
+            throw new Error('Stopwatch has not started');
+
+        running = false;
+
+        endTime = new Date();
+
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds;
+    };
+    this.reset = function () {
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+    };
+
+    // Read Only
+    Object.defineProperty(this, 'duration', {
+        get: function() {
+            return duration;
+        }
+    });
+}
