868841818c41827b0681180161e4c60572d0bcd1
dvru <damvaru4@gmail.com>
2021-01-18 17:08:12 -0600

    prototype bs instance

--- OOP/PrototypeVsInstance.js
+++ OOP/PrototypeVsInstance.js
@@ -5,9 +5,13 @@ function Circle(radius) {
     this.radius = radius;
 }
 // Prototype members
-Circle.prototype.draw = function () {
+Circle.prototype.draw = function() {
     console.log('draw');
 }
 
 const c1 = new Circle(1);
 const c2 = new Circle(1);
+
+Circle.prototype.toString = function() {
+    return 'Circle with radius ' + this.radius; 
+}
