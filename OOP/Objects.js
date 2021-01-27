3f8eab0782ba71d5a30cf03985b9e0caf49248ef
Damini Varu <damvaru4@gmail.com>
2021-01-26 20:57:44 -0600

    define a getter/setter

--- OOP/Objects.js
+++ OOP/Objects.js
@@ -60,6 +60,7 @@ const circle = {
     let defaultLocation = {};                      
  }                       
  
+ 
  // To define a getter/setter, use Object.defineProperty():
  
  Object.defineProperty(this, 'defaultLocation', {
